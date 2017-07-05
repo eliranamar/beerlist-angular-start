de4586e45c02d4a2bbd2d7c9158a936d7a79d93f
eliranamar <eliranamar7@gmail.com>
2017-07-05 11:56:03 +0300

    completed server

--- beerModel.js
+++ beerModel.js
@@ -1,1 +1,13 @@
+var mongoose = require('mongoose');
 
+var beerSchema = new mongoose.Schema({
+  name: String,
+  style: String,
+  image_url: String,
+  abv: Number,
+  ratings: [Number]
+});
+
+var Beer = mongoose.model('beer', beerSchema);
+
+module.exports = Beer;
